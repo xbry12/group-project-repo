97ee77be5f096923d8f3d0a43ed7e9fa41a8cfcb
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-11-13 19:16:53 -0500

    test

--- src/components/classes.test.js
+++ src/components/classes.test.js
@@ -10,7 +10,9 @@ describe('Classes component', () => {
     component = shallow(<Classes />)
   })
   
-  // add the rest of the tests here
+  it('should have a class name of class', () => {
+    expect(component.contains(<h1 className='class'></h1>)).toBe(true)
+  })
 
 })
 
