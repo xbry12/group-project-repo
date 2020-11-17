8f2eacd750f92f08e0a0bdfe57557de236cb33f1
Bryant Pyram <bryant.pyram@gmail.com>
2020-11-17 09:11:30 -0500

    dropdown

--- src/components/booking.js
+++ src/components/booking.js
@@ -2,10 +2,30 @@ import React, { Component } from "react";
 import "./booking.css";
 import {Button} from '../stories/Button'
 import Dropdown from "../dropdown";
+
+
 class Booking extends Component {
   constructor(props) {
     super()
+    this.state = {
   }
+}
+componentDidMount() {
+  let hello = []
+  this.props.instructors.forEach((person,i) => {
+    hello.push({id:person._id, value: `${person.firstName} ${person.lastName}`})
+    // console.log(person)
+  })
+  console.log(hello)
+  this.setState({
+    items: hello
+  })
+
+  // console.log(this.props.instructors)
+}
+
+
+
 
   render() {
     console.log(this.props.data)
@@ -17,13 +37,17 @@ class Booking extends Component {
             ? this.props.data.map((bookings) => 
             <div>
             <h2>{(bookings.day)}</h2>
-            <Button className='storybook-button--primary storybook-button--large' label='Booked'/></div>
+            <Button className='storybook-button--primary storybook-button--small' label='Booked'/></div>
             )
             
             : null}
            
         </div>
-        <Dropdown />
+
+        <div className="container">
+        <Dropdown title="select instructor" items={this.state.items} />
+
+        </div>
         {/* <form>
           <label>
             Enter Name
